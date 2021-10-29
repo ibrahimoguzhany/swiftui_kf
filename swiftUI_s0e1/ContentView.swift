858aedc7ae4a41d87fee29e5c6819d3dfca036e1
oguzhan <ibrahimoguzhany@gmail.com>
2021-10-30 00:04:05 +0300

    Initial Commit

---
+++ swiftUI_s0e1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI_s0e1
+//
+//  Created by İbrahim Oğuzhan Yılmaz on 30.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI_s0e1/swiftUI_s0e1App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_s0e1App.swift
+//  swiftUI_s0e1
+//
+//  Created by İbrahim Oğuzhan Yılmaz on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_s0e1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
